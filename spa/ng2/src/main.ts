4482fa8d549306f3ad50ec624843c54200c691d7
matt-newell <matt.newell@codescience.com>
2016-02-10 22:30:36 -0500

    switching to ng1

--- spa/ng2/src/main.ts
+++ spa/ng2/src/main.ts
@@ -1,10 +1,30 @@
-import { bootstrap } from 'angular2/platform/browser';
-import { HTTP_PROVIDERS } from 'angular2/http';
-import { ROUTER_PROVIDERS } from 'angular2/router';
-import { App } from './component/app';
-import { StoreService }  from './service/store-service';
-import { CartService }  from './service/cart-service';
-import './main.scss';
+var angular = require('angular');
+
+//import * as angular from 'angular';
+
+var app = angular.module('app', []);
+app.controller('DemoCtrl', function DemoCtrl() {
+    this.message = "Hallo Welt";
+});
+angular.element(document).ready(() => {
+    angular.bootstrap(document, ['app']);
+});
 
 
-bootstrap(App, [HTTP_PROVIDERS, ROUTER_PROVIDERS, StoreService, CartService]);
+
+
+var jsforce = require('jsforce');
+
+
+
+
+// import { bootstrap } from 'angular2/platform/browser';
+// import { HTTP_PROVIDERS } from 'angular2/http';
+// import { ROUTER_PROVIDERS } from 'angular2/router';
+// import { App } from './component/app';
+// import { StoreService }  from './service/store-service';
+// import { CartService }  from './service/cart-service';
+// import './main.scss';
+//
+//
+// bootstrap(App, [HTTP_PROVIDERS, ROUTER_PROVIDERS, StoreService, CartService]);
--- spa/ng2/src/vendor.ts
+++ spa/ng2/src/vendor.ts
@@ -1,5 +1,7 @@
-import 'angular2/bundles/angular2-polyfills';
-import 'angular2/platform/browser';
-import 'angular2/core';
-import 'angular2/http';
-import 'angular2/router';
+import 'angular';
+
+// import 'angular2/bundles/angular2-polyfills';
+// import 'angular2/platform/browser';
+// import 'angular2/core';
+// import 'angular2/http';
+// import 'angular2/router';

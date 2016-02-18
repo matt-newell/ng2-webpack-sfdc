ba69c217cdc1b6164ae3c23dd6c460b9c59a8ba0
Matt Newell <matt.newell@codescience.com>
2016-02-18 16:51:54 -0500

    cruft clean up

--- spa/ng1_bable/api/generate.js
+++ spa/ng1_bable/api/generate.js
@@ -1,6 +1,7 @@
 module.exports = function () {
     var faker = require( 'faker' );
     var _ = require( 'lodash' );
+    var getUserInfo = require('./mocks/getUserInfo.js');
 
     var productName = function(){
         return faker.commerce.productAdjective() +
@@ -13,15 +14,19 @@ module.exports = function () {
         return faker.image.imageUrl();
     };
 
+    // This is your json structurn each named object below can match a remote call
     return {
-        getUserInfo: {
-            avatar: faker.internet.avatar(),
-            firstName: faker.name.firstName(),
-            id: "00561000000L6lTAAS",
-            lastName: faker.name.lastName(),
-            name: faker.name.findName()
-        },
-        yo: _.times( 6, function ( id ) {
+        getUserInfo: getUserInfo,
+        getSidebar: _.times(3, function( id ) {
+            return {
+                id: id+1,
+                name: "Home",
+                icon: "home",
+                iconBackground: faker.internet.color(),
+                sequence: id+1
+            }
+        } ),
+        products: _.times( 9, function ( id ) {
             var title = productName();
             return {
                 id: id+1,
@@ -32,7 +37,7 @@ module.exports = function () {
                 summary: faker.lorem.paragraph()
             }
         } ),
-        products: _.times( 9, function ( id ) {
+        products6: _.times( 6, function ( id ) {
             var title = productName();
             return {
                 id: id+1,
--- spa/ng1_bable/src/mocks.js
+++ spa/ng1_bable/src/mocks.js
@@ -12,48 +12,16 @@ var configSettings = configSettings || {},
 
 configSettings = {
     staticPath: '',
-    config: {
-                        hero : '72,69,82,79',
-                   heroGiphy : '//giphy.com/embed/13yLVC3RhV5rY4'
-                     ////giphy.com/embed/zybS9Wuvgh57y
-                     ////giphy.com/embed/HTvhG3r0EZEas
-    },
+    config: {  },
     remoteActions: {
-                  getSidebar : '{!$RemoteAction.AlfredHomeController.getSidebar}',
-                 getUserInfo : '{!$RemoteAction.AlfredHomeController.getUserInfo}',
-                          yo : '{!$Testy.say.yo.me.newell}'
+        getSidebar      : '{!$RemoteAction.AlfredHomeController.getSidebar}',
+        getUserInfo     : '{!$RemoteAction.AlfredHomeController.getUserInfo}',
+        yo              : '{!$Testy.say.yo.me.newell}'
     },
     mocks: {
-        '{!$RemoteAction.AlfredHomeController.getSidebar}' : {
-            timeout : 500,
-            method  : function () {
-                return [
-                    {
-                        "id": "A0gjkjfkdsjfkdsf",
-                        "name": "Home",
-                        "icon": "home",
-                        "iconBackground": "rgb(114, 100, 236)",
-                        "sequence": 0
-                    },
-                    {
-                        "id": "B99dsfljlkjlkjsdf",
-                        "name": "Favorites",
-                        "icon": "favorite",
-                        "iconBackground": "rgb(232, 82, 126)",
-                        "sequence": 1
-                    },
-                    {
-                        "id": "C4asdflkj2lkjsf",
-                        "name": "Trails",
-                        "icon": "trails",
-                        "iconBackground": "rgb(51, 113, 234)",
-                        "sequence": 2
-                    }
-                ];
-            }
-        },
+        '{!$RemoteAction.AlfredHomeController.getSidebar}' : 'getSidebar',
         '{!$RemoteAction.AlfredHomeController.getUserInfo}' : 'getUserInfo',
-        '{!$Testy.say.yo.me.newell}': 'yo',
+        '{!$Testy.say.yo.me.newell}': 'products6',
         //String value of your mock matches json object on json-server
         '{!$Testy.say.yo.me.newell.Simple.Object}': {
             timeout : 5000,
